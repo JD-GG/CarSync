5a5f4f0b9c90dbfe6db20e7e357688ff5a8baa78
JD-GG <jandavid555@gmail.com>
2025-08-22 12:40:13 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "CarSync Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+    outputs = { self, nixpkgs }: 
+        let 
+            pkgs = import nixpkgs { 
+              system = "x86_64-linux"; 
+              config.allowUnfree = true;
+            };
+            system = "x86_64-linux";
+        in {
+            devShells.x86_64-linux.default = pkgs.mkShell {
+                buildInputs = [
+                    pkgs.docker
+                    pkgs.docker-compose
+                    pkgs.nodejs
+                    pkgs.arduino-cli
+                    pkgs.arduino-ide
+                ];
+
+                shellHook = ''
+                  alias up='docker-compose up --build -d'
+                  alias down='docker-compose down'
+                  clean() {
+                    docker-compose down
+                    docker rm -f $(docker ps -aq)
+                    docker rmi -f $(docker images -q)
+                    docker volume rm $(docker volume ls -q)
+                  }
+                  alias mqtt='nohup mqtt-explorer &'
+                  ide() {
+                    nohup arduino-ide --disable-gpu ~/Documents/GitHub/audiTim/Arduino/edgeDevice/edgeDevice.ino > /dev/null 2>&1 &
+                    disown
+                  }
+                  arduino-cli lib list
+                '';
+            };
+        };
+}
