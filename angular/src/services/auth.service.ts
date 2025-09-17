6c3c7ae4eae894f01693b4551207201e7793ad7b
JD-GG <104784838+JD-GG@users.noreply.github.com>
2025-09-17 13:27:33 +0200

    revert changes. Fixed in reverse proxy

--- angular/src/services/auth.service.ts
+++ angular/src/services/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private apiUrl = 'http://backend:3000/api';
+  private apiUrl = '/api';  
 
   constructor(private http: HttpClient) {}
 
